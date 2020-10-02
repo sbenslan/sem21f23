649e7f2350d6bc0ecf11f4398d672c1100b06be1
Matteo Spallanzani <spmatteo@iis.ee.ethz.ch>
2020-10-02 12:53:56 +0200

    In object oriented programming, deciding between parametrising a constructor method and subclassing an existing class can be tricky. Personally, in the context of PyTorch networks I think that changing the parameters of existing blocks should be treated by declining the constructor method, whereas changing the topology (e.g., by replacing a layer with a stack of simpler layers) requires subclassing.

--- problems/ImageNet/ResNet/resnet.py
+++ problems/ImageNet/ResNet/resnet.py
@@ -1,116 +1,118 @@
-import os
 import math
-from collections import OrderedDict
-
 import torch
 import torch.nn as nn
-import torch.utils.model_zoo as model_zoo
-
-try:
-    from torch.hub import load_state_dict_from_url
-except ImportError:
-    from torch.utils.model_zoo import load_url as load_state_dict_from_url
-
-# model_urls = {
-#     'se_resnet26': 'https://data.vision.ee.ethz.ch/kmaninis/share/MTL//models/se_resnet26-5eb336d20.pth',
-#     'se_resnet50': 'https://data.vision.ee.ethz.ch/kmaninis/share/MTL//models/se_resnet50-ad8889f9f.pth',
-#     'se_resnet101': 'https://data.vision.ee.ethz.ch/kmaninis/share/MTL//models/se_resnet101-8dbb64f8e.pth',
-# }
-# TODO: Move this to config
-
-__all__ = ['ResNet', 'resnet26', 'resnet50', 'resnet101']
 
 
-models_dir = './models'
+__all__ = ['ResNet', 'ResNet3x3']
 
-model_urls = {
-    'resnet50': 'https://download.pytorch.org/models/resnet50-19c8e357.pth',
-    'resnet101': 'https://download.pytorch.org/models/resnet101-5d3b4d8f.pth',
+
+__CONFIGS__ = {
+    'ResNet26': [2, 2, 2, 2],
+    'ResNet50': [3, 4, 6, 3],
+    'ResNet101': [3, 4, 23, 3],
 }
 
 
 class Bottleneck(nn.Module):
-    expansion = 4
 
-    def __init__(self, inplanes, planes, stride=1, downsample=None):
+    expansion = 4  # this is hardcoded for every ResNet, and should not be changed!
+
+    def __init__(self, in_channels, med_channels, stride=1, downsample=None):
         super(Bottleneck, self).__init__()
-        expansion = 4
-        self.stride = stride
+        out_channels = med_channels * self.expansion
 
-        # channel compression
-        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(planes)
-        self.relu1 = nn.ReLU(inplace=True)
-
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=self.stride, padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(planes)
-        self.relu2 = nn.ReLU(inplace=True)
-
-        # channel decompression
-        self.conv3 = nn.Conv2d(planes, planes * expansion, kernel_size=1, bias=False)
-        self.bn3 = nn.BatchNorm2d(planes * expansion)
+        # skip branch transform
         self.downsample = downsample
+
+        # residual branch transform
+        # channel compression
+        self.conv1 = nn.Conv2d(in_channels, med_channels, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(med_channels)
+        self.relu1 = nn.ReLU(inplace=True)
+        # spatial convolutions
+        self.conv2 = nn.Conv2d(med_channels, med_channels, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(med_channels)
+        self.relu2 = nn.ReLU(inplace=True)
+        # channel decompression
+        self.conv3 = nn.Conv2d(med_channels, out_channels, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(out_channels)
+
         self.relu3 = nn.ReLU(inplace=True)
 
     def forward(self, x):
-        residual = x
 
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu1(out)
-
-        out = self.conv2(out)
-        out = self.bn2(out)
-        out = self.relu2(out)
-
-        out = self.conv3(out)
-        out = self.bn3(out)
+        # skip branch
+        xs = x
         if self.downsample is not None:
-            residual = self.downsample(x)
-        out += residual
-        out = self.relu3(out)
+            xs = self.downsample(xs)
 
-        return out
+        # residual branch
+        xr = self.conv1(x)
+        xr = self.bn1(xr)
+        xr = self.relu1(xr)
+        xr = self.conv2(xr)
+        xr = self.bn2(xr)
+        xr = self.relu2(xr)
+        xr = self.conv3(xr)
+        xr = self.bn3(xr)
+
+        # join
+        x = torch.add(xs, xr)
+        x = self.relu3(x)
+
+        return x
 
 
 class ResNet(nn.Module):
 
-    def __init__(self, layers, num_classes=1000, socket3x3=False):
-        self.inplanes = 64
+    def __init__(self, config, num_classes=1000):
+
+        self.in_channels = 64
         block = Bottleneck
+
         super(ResNet, self).__init__()
 
-        if not socket3x3:
-            adapter = nn.Sequential(
-                nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False),
-                nn.BatchNorm2d(64),
-                nn.ReLU(inplace=True)
-            )
-        else:
-            adapter = nn.Sequential(
-                nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False),
-                nn.BatchNorm2d(64),
-                nn.ReLU(inplace=True),
-                nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1, bias=False),
-                nn.BatchNorm2d(64),
-                nn.ReLU(inplace=True),
-                nn.Conv2d(64, 64, kernel_size=3, stride=2, padding=1, bias=False),
-                nn.BatchNorm2d(64),
-                nn.ReLU(inplace=True)
-            )
-        self.socket = adapter
-
+        # adapter
+        self.adapter = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True)
+        )
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
 
-        self.layer1 = self._make_layer(block, 64, layers[0])
-        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
-        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
+        # feature extractor
+        self.layer1 = self._make_layer(block, 64,  __CONFIGS__[config][0])
+        self.layer2 = self._make_layer(block, 128, __CONFIGS__[config][1], stride=2)
+        self.layer3 = self._make_layer(block, 256, __CONFIGS__[config][2], stride=2)
+        self.layer4 = self._make_layer(block, 512, __CONFIGS__[config][3], stride=2)
 
+        # classifier
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-
         self.fc = nn.Linear(512 * block.expansion, num_classes)
 
+        self._initialise_params()
+
+    def _make_layer(self, block, med_channels, num_blocks, stride=1):
+        out_channels = med_channels * block.expansion
+
+        # first block
+        if stride == 1 and self.in_channels == out_channels:
+            downsample = None
+        if stride != 1 or self.in_channels != out_channels:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.in_channels, out_channels, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(out_channels),
+            )
+        blocks = [block(self.in_channels, med_channels, stride, downsample)]
+        self.in_channels = out_channels
+
+        # remaining blocks
+        for i in range(1, num_blocks):
+            blocks.append(block(self.in_channels, med_channels))
+
+        return nn.Sequential(*blocks)
+
+    def _initialise_params(self):
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
                 n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
@@ -119,25 +121,9 @@ class ResNet(nn.Module):
                 m.weight.data.fill_(1)
                 m.bias.data.zero_()
 
-    def _make_layer(self, block, planes, blocks, stride=1):
-        downsample = None
-        if stride != 1 or self.inplanes != planes * block.expansion: # planes * block.expansion = outplanes; in Bottleneck, block.expansion seems hard-coded to 4
-            downsample = nn.Sequential(
-                nn.Conv2d(self.inplanes, planes * block.expansion, kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(planes * block.expansion),
-            )
-
-        layers = []
-        layers.append(block(self.inplanes, planes, stride, downsample))
-        self.inplanes = planes * block.expansion
-        for i in range(1, blocks):
-            layers.append(block(self.inplanes, planes))
-
-        return nn.Sequential(*layers)
-
     def forward(self, x):
-        x = self.socket(x)
 
+        x = self.adapter(x)
         x = self.maxpool(x)
 
         x = self.layer1(x)
@@ -146,84 +132,34 @@ class ResNet(nn.Module):
         x = self.layer4(x)
 
         x = self.avgpool(x)
-
         x = x.view(x.size(0), -1)
         x = self.fc(x)
 
         return x
 
 
-def get_state_dict(model_name, remote=False):
-    # find and load checkpoint
-    if remote:
-        raise NotImplementedError
-    else:
-        checkpoint = torch.load(os.path.join(models_dir, model_name + '.pth'), map_location=lambda storage, loc: storage)
-    checkpoint = checkpoint['state_dict']
-
-    # handle DataParallel
-    if 'module.' in list(checkpoint.keys())[0]:
-        new_state_dict = OrderedDict()
-        for k, v in checkpoint.items():
-            name = k.replace('module.', '')  # remove `module.`
-            new_state_dict[name] = v
-    else:
-        new_state_dict = checkpoint
-
-    return new_state_dict
-
-
-def resnet26(pretrained=False, **kwargs):
-    """Construct a ResNet-26 model.
-
-    Args:
-        pretrained (bool): If True, returns a model pre-trained on ImageNet.
-        depthwise (bool): If True, create a model with depthwise separable convolutions.
-    """
-    model = ResNet(Bottleneck, [2, 2, 2, 2], **kwargs)
-
-    if pretrained:
-        print('Loading ResNet-26 ImageNet...')
-        model_name = 'resnet26'
-        state_dict = get_state_dict(model_name)
-        # Load pre-trained IN model
-        model.load_state_dict(state_dict)
-        print('\b done!')
-
-    return model
-
-
-def resnet50(pretrained=False, **kwargs):
-    """Construct a ResNet-50 model.
-
-    Args:
-        pretrained (bool): If True, returns a model pre-trained on ImageNet.
-        depthwise (bool): If True, create a model with depthwise separable convolutions.
+class ResNet3x3(ResNet):
     """
 
-    model = ResNet(Bottleneck, [3, 4, 6, 3], **kwargs)
-
-    if pretrained:
-        print('Loading ResNet-50 ImageNet...')
-        model.load_state_dict(model_zoo.load_url(model_urls['resnet50']))
-        print('\b done!')
-
-    return model
-
-
-def resnet101(pretrained=False, **kwargs):
-    """Construct a ResNet-101 model.
-
-    Args:
-        pretrained (bool): If True, returns a model pre-trained on ImageNet.
-        depthwise (bool): If True, create a model with depthwise separable convolutions.
+    Basic ResNets use a single convolutional layer whose filters have 7x7
+    spatial dimensions. This variant will use instead a stack of convolutional
+    layers whose filters have 3x3 spatial dimensions.
     """
 
-    model = ResNet(Bottleneck, [3, 4, 23, 3], **kwargs)
+    def __init__(self, adapter_size, config, num_classes=1000):
+        super(ResNet3x3, self).__init__(config, num_classes=num_classes)
 
-    if pretrained:
-        print('Loading ResNet-101 ImageNet...')
-        model.load_state_dict(model_zoo.load_url(model_urls['resnet101']))
-        print('\b done!')
+        adapter = [
+            nn.Conv2d(3, 64, kernel_size=3, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True)
+        ]
+        for i in range(1, adapter_size):
+            adapter.extend([
+                nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1, bias=False),
+                nn.BatchNorm2d(64),
+                nn.ReLU(inplace=True)
+            ])
+        self.adapter = nn.Sequential(*adapter)
 
-    return model
+        self._initialise_params()
