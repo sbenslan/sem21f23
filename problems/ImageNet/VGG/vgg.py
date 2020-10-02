827b03b73c48e23d03ec85b45c51bd8ffa108067
Matteo Spallanzani <spmatteo@iis.ee.ethz.ch>
2020-10-02 12:54:38 +0200

    I just wanted to make the code for the VGG class cleaner.

--- problems/ImageNet/VGG/vgg.py
+++ problems/ImageNet/VGG/vgg.py
@@ -1,4 +1,3 @@
-import torch
 import torch.nn as nn
 
 
@@ -13,21 +12,28 @@ __CONFIGS__ = {
 
 
 class VGG(nn.Module):
+
     def __init__(self, config, bn=False, num_classes=1000):
         super(VGG, self).__init__()
-        if bn:
-            adapter = nn.Sequential(
-                nn.Conv2d(3, 64, kernel_size=3, padding=1, bias=not bn),
+
+        # adapter
+        self.adapter = \
+            nn.Sequential(
+                nn.Conv2d(3, 64, kernel_size=3, padding=1, bias=False),
                 nn.BatchNorm2d(64),
                 nn.ReLU(inplace=True)
-            )
-        else:
-            adapter = nn.Sequential(
+            ) \
+            if bn \
+            else \
+            nn.Sequential(
                 nn.Conv2d(3, 64, kernel_size=3, padding=1),
                 nn.ReLU(inplace=True)
             )
-        self.adapter = adapter
+
+        # feature extractor
         self.features = self._make_features(__CONFIGS__[config], bn)
+
+        # classifier
         self.avgpool = nn.AdaptiveAvgPool2d((7, 7))
         self.classifier = nn.Sequential(
             nn.Linear(512 * 7 * 7, 4096),
@@ -38,7 +44,8 @@ class VGG(nn.Module):
             nn.Dropout(),
             nn.Linear(4096, num_classes),
         )
-        self._initialize_weights()
+
+        self._initialize_params()
 
     @staticmethod
     def _make_features(config, bn):
@@ -54,17 +61,7 @@ class VGG(nn.Module):
                 in_channels = v
         return nn.Sequential(*layers)
 
-    def forward(self, x):
-        x = self.adapter(x)
-        x = self.features(x)
-        x = self.avgpool(x)
-        # x = torch.flatten(x, 1)
-        x = x.view(x.size(0), -1)  # enable conversion to Caffe model; to understand difference see:
-                                   # https://stackoverflow.com/questions/57234095/what-is-the-difference-of-flatten-and-view-1-in-pytorch
-        x = self.classifier(x)
-        return x
-
-    def _initialize_weights(self):
+    def _initialize_params(self):
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
                 nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
@@ -76,3 +73,13 @@ class VGG(nn.Module):
             elif isinstance(m, nn.Linear):
                 nn.init.normal_(m.weight, 0, 0.01)
                 nn.init.constant_(m.bias, 0)
+
+    def forward(self, x):
+        x = self.adapter(x)
+        x = self.features(x)
+        x = self.avgpool(x)
+        # x = torch.flatten(x, 1)
+        x = x.view(x.size(0), -1)  # enable conversion to Caffe model; to understand difference see:
+                                   # https://stackoverflow.com/questions/57234095/what-is-the-difference-of-flatten-and-view-1-in-pytorch
+        x = self.classifier(x)
+        return x
