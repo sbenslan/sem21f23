95f9a75d925daf3301d69ce4503a60fd5445f8f2
Matteo Spallanzani <spmatteo@iis.ee.ethz.ch>
2021-01-13 18:20:41 +0100

    I implemented the ANA versions of the linear operations commonly used in feedforward networks (dense, convolutional).

--- quantlib/algorithms/ana/ana_ops.py
+++ quantlib/algorithms/ana/ana_ops.py
@@ -69,12 +69,6 @@ class ANAModule(nn.Module):
         anamod.register_parameter('bsigma', nn.Parameter(torch.ones(1), requires_grad=False))
         anamod.bnoise = Noise(mu=anamod.bmu, sigma=anamod.bsigma)
 
-
-class ANAActivation(ANAModule):
-    """Quantize scores."""
-    def __init__(self, quantizer_spec, noise_type):
-        super(ANAActivation, self).__init__(quantizer_spec, noise_type)
-
     def set_fnoise(self, mu, sigma):
         self.fnoise.mu.data = torch.Tensor([mu]).to(self.fnoise.mu)
         self.fnoise.sigma.data = torch.Tensor([sigma]).to(self.fnoise.sigma)
@@ -83,7 +77,14 @@ class ANAActivation(ANAModule):
         self.bnoise.mu.data = torch.Tensor([mu]).to(self.bnoise.mu)
         self.bnoise.sigma.data = torch.Tensor([sigma]).to(self.bnoise.sigma)
 
+
+class ANAActivation(ANAModule):
+    """Quantize scores."""
+    def __init__(self, quantizer_spec, noise_type):
+        super(ANAActivation, self).__init__(quantizer_spec, noise_type)
+
     def forward(self, x):
+
         x = x / self.eps if self.eps != 1.0 else x
 
         x_out = self.ana_op(x,
@@ -93,25 +94,20 @@ class ANAActivation(ANAModule):
                             self.training)
 
         x_out = x_out * self.eps if self.eps != 1.0 else x_out
+
         return x_out
 
 
-class ANALinear(nn.Module):
+class ANALinear(ANAModule):
     """Affine transform with quantized parameters."""
-    def __init__(self, noise_type, thresholds, quant_levels, in_features, out_features, bias=True):
-        super(ANALinear, self).__init__()
-        # set stochastic properties
-        self.ana_op = getattr(ana_lib, 'ANA' + noise_type.capitalize()).apply
-        super(ANALinear, self).register_parameter('thresholds',
-                                                  nn.Parameter(torch.Tensor(thresholds),
-                                                  requires_grad=False))
-        super(ANALinear, self).register_parameter('quant_levels',
-                                                  nn.Parameter(torch.Tensor(quant_levels),
-                                                  requires_grad=False))
-        super(ANALinear, self).register_parameter('stddev',
-                                                  nn.Parameter(torch.Tensor(torch.ones(2)),
-                                                  requires_grad=False))
-        # set linear layer properties
+    def __init__(self, quantizer_spec, noise_type,
+                 in_features, out_features, bias=True):
+
+        # set quantizer + ANA properties
+        super(ANALinear, self).__init__(quantizer_spec, noise_type)
+        assert self.eps == 1.0  # quantized weights should be "pure" integers, not fixed-point numbers
+
+        # set linear layer parameters
         self.in_features  = in_features
         self.out_features = out_features
         self.weight       = nn.Parameter(torch.Tensor(out_features, in_features))
@@ -122,40 +118,39 @@ class ANALinear(nn.Module):
         self.reset_parameters()
 
     def reset_parameters(self):
+
         stdv = 1. / math.sqrt(self.weight.size(1))
+
         # init weights near thresholds
         self.weight.data.random_(to=len(self.thresholds.data))
         self.weight.data = self.thresholds[self.weight.data.to(torch.long)]
         self.weight.data = torch.add(self.weight.data, torch.zeros_like(self.weight.data).uniform_(-stdv, stdv))
+
         # init biases
         if self.bias is not None:
             self.bias.data.uniform_(-stdv, stdv)
 
-    def set_stddev(self, stddev):
-        self.stddev.data = torch.Tensor(stddev).to(self.stddev)
-
     def forward(self, input):
-        weight = self.activate_weight(self.weight, self.thresholds, self.quant_levels, self.stddev, self.training)
+
+        weight = self.ana_op(self.weight,
+                             self.quant_levels, self.thresholds,
+                             self.fnoise.mu, self.fnoise.sigma,
+                             self.bnoise.mu, self.bnoise.sigma,
+                             self.training)
+
         return F.linear(input, weight, self.bias)
 
 
-class _ANAConvNd(nn.Module):
+class _ANAConvNd(ANAModule):
     """Cross-correlation transform with quantized parameters."""
-    def __init__(self, noise_type, thresholds, quant_levels,
+    def __init__(self, quantizer_spec, noise_type,
                  in_channels, out_channels, kernel_size, stride, padding, dilation, transposed, output_padding, groups, bias):
-        super(_ANAConvNd, self).__init__()
-        # set stochastic properties
-        self.ana_op = getattr(ana_lib, 'ANA' + noise_type.capitalize()).apply
-        super(_ANAConvNd, self).register_parameter('thresholds',
-                                                   nn.Parameter(torch.Tensor(thresholds),
-                                                   requires_grad=False))
-        super(_ANAConvNd, self).register_parameter('quant_levels',
-                                                   nn.Parameter(torch.Tensor(quant_levels),
-                                                   requires_grad=False))
-        super(_ANAConvNd, self).register_parameter('stddev',
-                                                   nn.Parameter(torch.Tensor(torch.ones(2)),
-                                                   requires_grad=False))
-        # set convolutional layer properties
+
+        # set quantizer + ANA properties
+        super(_ANAConvNd, self).__init__(quantizer_spec, noise_type)
+        assert self.eps == 1.0  # quantized weights should be "pure" integers, not fixed-point numbers
+
+        # set convolutional layer parameters
         if in_channels % groups != 0:
             raise ValueError('in_channels must be divisible by groups')
         if out_channels % groups != 0:
@@ -182,65 +177,92 @@ class _ANAConvNd(nn.Module):
         self.reset_parameters()
 
     def reset_parameters(self):
+
         n = self.in_channels
         for k in self.kernel_size:
             n *= k
         stdv = 1. / math.sqrt(n)
+
         # init weights near thresholds
         self.weight.data.random_(to=len(self.thresholds.data))
         self.weight.data = self.thresholds[self.weight.data.to(torch.long)]
         self.weight.data = torch.add(self.weight.data, torch.zeros_like(self.weight.data).uniform_(-stdv, stdv))
+
         # init biases
         if self.bias is not None:
             self.bias.data.uniform_(-stdv, stdv)
 
-    def set_stddev(self, stddev):
-        self.stddev.data = torch.Tensor(stddev).to(self.stddev)
-
 
 class ANAConv1d(_ANAConvNd):
-    def __init__(self, noise_type, thresholds, quant_levels,
+    def __init__(self, quantizer_spec, noise_type,
                  in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, groups=1, bias=True):
+
         kernel_size = _single(kernel_size)
         stride      = _single(stride)
         padding     = _single(padding)
         dilation    = _single(dilation)
+
         super(ANAConv1d, self).__init__(
-              noise_type, thresholds, quant_levels,
-              in_channels, out_channels, kernel_size, stride, padding, dilation, False, _single(0), groups, bias)
+            quantizer_spec, noise_type,
+            in_channels, out_channels, kernel_size, stride, padding, dilation, False, _single(0), groups, bias
+        )
 
     def forward(self, input):
-        weight = self.ana_op(self.weight, self.thresholds, self.quant_levels, self.stddev, self.training)
+
+        weight = self.ana_op(self.weight,
+                             self.quant_levels, self.thresholds,
+                             self.fnoise.mu, self.fnoise.sigma,
+                             self.bnoise.mu, self.bnoise.sigma,
+                             self.training)
+
         return F.conv1d(input, weight, self.bias, self.stride, self.padding, self.dilation, self.groups)
 
 
 class ANAConv2d(_ANAConvNd):
-    def __init__(self, noise_type, thresholds, quant_levels,
+    def __init__(self, quantizer_spec, noise_type,
                  in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, groups=1, bias=True):
+
         kernel_size = _pair(kernel_size)
         stride      = _pair(stride)
         padding     = _pair(padding)
         dilation    = _pair(dilation)
+
         super(ANAConv2d, self).__init__(
-              noise_type, thresholds, quant_levels,
-              in_channels, out_channels, kernel_size, stride, padding, dilation, False, _pair(0), groups, bias)
+            quantizer_spec, noise_type,
+            in_channels, out_channels, kernel_size, stride, padding, dilation, False, _pair(0), groups, bias
+        )
 
     def forward(self, input):
-        weight = self.ana_op(self.weight, self.thresholds, self.quant_levels, self.stddev, self.training)
+
+        weight = self.ana_op(self.weight,
+                             self.quant_levels, self.thresholds,
+                             self.fnoise.mu, self.fnoise.sigma,
+                             self.bnoise.mu, self.bnoise.sigma,
+                             self.training)
+
         return F.conv2d(input, weight, self.bias, self.stride, self.padding, self.dilation, self.groups)
 
 
 class ANAConv3d(_ANAConvNd):
-    def __init__(self, noise_type, thresholds, quant_levels,
+    def __init__(self, quantizer_spec, noise_type,
                  in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, groups=1, bias=True):
+
         kernel_size = _triple(kernel_size)
         stride      = _triple(stride)
         padding     = _triple(padding)
         dilation    = _triple(dilation)
+
         super(ANAConv3d, self).__init__(
-              noise_type, thresholds, quant_levels,
-              in_channels, out_channels, kernel_size, stride, padding, dilation, False, _triple(0), groups, bias)
+            quantizer_spec, noise_type,
+            in_channels, out_channels, kernel_size, stride, padding, dilation, False, _triple(0), groups, bias
+        )
 
     def forward(self, input):
-        weight = self.ana_op(self.weight, self.thresholds, self.quant_levels, self.stddev, self.training)
+
+        weight = self.ana_op(self.weight,
+                             self.quant_levels, self.thresholds,
+                             self.fnoise.mu, self.fnoise.sigma,
+                             self.bnoise.mu, self.bnoise.sigma,
+                             self.training)
+
         return F.conv3d(input, weight, self.bias, self.stride, self.padding, self.dilation, self.groups)
