3501488c59a27ed6e05710aab7c762face023e9e
sbenslan <sbenslan@student.ethz.ch>
2021-03-30 11:08:49 +0200

    typo

--- problems/CIFAR-10/VGG/__init__.py
+++ problems/CIFAR-10/VGG/__init__.py
@@ -1,7 +1,4 @@
 from .preprocess import load_data_sets
 from .postprocess import postprocess_pr, postprocess_gt
-
-from .loss_functions import HingeLoss
-
-from .vggbaseline import VGGBaseline
-from .vgg import VGG
+from .vgg import *
+from .quantize import *
